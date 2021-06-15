231c704ef1491a988060402cd74e90a856204314
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-06-15 20:09:48 +0300

    Keep x variable in localStoreg

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
-var x = 1;
+function something() 
+{
+	var x = window.localStorege.getItem('bbb');
+	x = x * 1 + 1;
 
-function something() {
-	alert (x);
-	x = x + 1
+	window.localStorege.setItem('bbb', x);
+
+	alert(x);
 }
