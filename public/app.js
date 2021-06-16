a26405c2bd452d990e3eb059804ab17cb4e48b84
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-06-16 22:54:12 +0300

    Add update_orders_button

--- public/app.js
+++ public/app.js
@@ -17,6 +17,7 @@ function add_to_cart(id)
 	window.localStorage.setItem(key, x);	
 
 	update_orders_input();
+	update_orders_button();
 }
 
 function cart_get_namber_of_items()
@@ -43,6 +44,11 @@ function update_orders_input()
 	$('#orders_input').val(orders);
 }
 
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_namber_of_items() + ')'
+	$('#orders_button').val(text);
+}
 
 function cart_get_orders()
 {
