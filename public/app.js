2faed5b2d0f94a6f07a95b9812f69d04bf7da08b
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-06-16 22:11:21 +0300

    Add cart_get_orders

--- public/app.js
+++ public/app.js
@@ -21,6 +21,8 @@ function add_to_cart(id)
 
 function cart_get_namber_of_items()
 {
+	var cnt = 0;
+
 	for(var i = 0; i < window.localStorage.length; i++)
 	{
 		var key = window.localStorage.key(i);
@@ -33,4 +35,24 @@ function cart_get_namber_of_items()
 	}
 
 	return cnt;
+}
+
+function
+
+function cart_get_orders()
+{
+	var orders = '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+	
+		if(key.indexOf('product_') == 0) 
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}
+
+	return orders;
 }
