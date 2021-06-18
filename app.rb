dd406047ae3b63f1f3df978b9d4be6bf06830742
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-06-18 23:26:09 +0300

    Add comment in app.rb

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+
+#Worked version
 set :database, {adapter:"sqlite3", database:"pizzashop.db"}
 
 class Product < ActiveRecord::Base
