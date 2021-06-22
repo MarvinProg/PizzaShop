ed58d3c397b819c2a175dd496c56047aaf589c28
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-06-22 12:01:56 +0300

    Corrected error in POST /place order

--- app.rb
+++ app.rb
@@ -17,24 +17,18 @@ end
 
 def parse_orders_input orders_input
 
-  s1 = orders_input.split(/,/)
-    
+  s1 = orders_input.split(',')
   arr = []
   
   s1.each do |x| 
-    s2 = x.split(/\=/)
-
-    s3 = s2[0].split(/_/)
-  
+    s2 = x.split("=")
+    s3 = s2[0].split('_')
     id = s3[1]
     cnt = s2[1]
-    
     arr2 = [id, cnt]
     arr.push arr2
   end
-
   return arr
-  
 end
 
 get '/' do
@@ -46,6 +40,13 @@ get '/about' do
   erb :about
 end
 
+post '/place_order' do
+  #@order = Order.new params[:order]
+
+  @order = Order.create params[:order]
+  erb "Thanks you! Your order has been placed."
+end
+
 post '/cart' do
   @orders_input = params[:orders_input]
   @items = parse_orders_input @orders_input
@@ -53,13 +54,13 @@ post '/cart' do
   @items.each do |item|
     #id, cnt
     item[0] = Product.find(item[0])
-
   end
 
   @o = {}
-
   erb :cart
 end
+
+
 
 configure do
   enable :sessions
